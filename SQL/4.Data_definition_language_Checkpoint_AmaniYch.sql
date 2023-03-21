9926facf54134d6373b869bc0c4b80a10e0f6f66
Amani Hanane YAICHE <127974682+ahyaiche@users.noreply.github.com>
2023-03-21 17:59:45 +0100

    Update 4.Data_definition_language_Checkpoint_AmaniYch.sql

--- SQL/4.Data_definition_language_Checkpoint_AmaniYch.sql
+++ SQL/4.Data_definition_language_Checkpoint_AmaniYch.sql
@@ -1,4 +1,3 @@
-```sql
 ## customers table has a “primary key” column customer_id, and the name, email, and address columns are all defined as “VARCHAR” with different maximum lengths and each is 
 #marked as “NOT NULL”.
 ## products table has a “primary key” column product_id, and the name and price columns are defined as “VARCHAR” and “DECIMAL” respectively, 
@@ -48,4 +47,3 @@ CREATE TABLE orders (
 desc customers;    
 desc products;
 desc orders;
-```
