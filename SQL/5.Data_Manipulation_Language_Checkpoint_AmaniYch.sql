4ba0b365c7888002dec396fee3c294807f2f7f91
Amani Hanane YAICHE <127974682+ahyaiche@users.noreply.github.com>
2023-03-22 07:04:15 +0100

    Update 5.Data_Manipulation_Language_Checkpoint_AmaniYch.sql

--- SQL/5.Data_Manipulation_Language_Checkpoint_AmaniYch.sql
+++ SQL/5.Data_Manipulation_Language_Checkpoint_AmaniYch.sql
@@ -31,22 +31,14 @@ CREATE TABLE orders (
 
 # Fill the values of each table
 # 1. customers table
-insert into customers values (1, 'Alice', '123 Main St.');
-insert into customers values (2, 'Bob', '456 Market St.');
-insert into customers values (3, 'Charlie', '789 Elm St.');
+insert into customers values (1, 'Alice', '123 Main St.'), (2, 'Bob', '456 Market St.'), (3, 'Charlie', '789 Elm St.');
 
 # 2. products table
-insert into products values (1, 'Widget', 10.00);
-insert into products values (2, 'Gadget', 20.00);
-insert into products values (3, 'Doohickey', 15.00);
+insert into products values (1, 'Widget', 10.00), (2, 'Gadget', 20.00), (3, 'Doohickey', 15.00);
 
 # 3. orders table
-insert into orders values (1, 1, 1, 10, '2021-01-01');
-insert into orders values (2, 1, 2, 5, '2021-01-02');
-insert into orders values (3, 2, 1, 3, '2021-01-03');
-insert into orders values (4, 2, 2, 7, '2021-01-04');
-insert into orders values (5, 3, 1, 2, '2021-01-05');
-insert into orders values (6, 3, 3, 3, '2021-01-06');
+insert into orders values (1, 1, 1, 10, '2021-01-01'), (2, 1, 2, 5, '2021-01-02'), (3, 2, 1, 3, '2021-01-03'), 
+							(4, 2, 2, 7, '2021-01-04'), (5, 3, 1, 2, '2021-01-05'), (6, 3, 3, 3, '2021-01-06');
 
 # 4. Display the filled tables
 select * from customers;    
